a69d5de69af1c786a5d02f783935a0db37c59a28
unknown <derekmclean.mtl@gmail.com>
2017-09-01 16:25:15 -0400

    MainActivity checks for empty string now

--- app/src/main/java/ca/campbell/simplecalc/MainActivity.java
+++ app/src/main/java/ca/campbell/simplecalc/MainActivity.java
@@ -1,10 +1,16 @@
 package ca.campbell.simplecalc;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 //  TODO: add buttons & methods for subtract, multiply, divide
 //  TODO: extra input validation: no divide by zero
@@ -17,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     EditText etNum1, etNum2;
     TextView result;
     double num1, num2;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +37,54 @@ public class MainActivity extends AppCompatActivity {
         etNum1 = (EditText) findViewById(R.id.num1);
         etNum2 = (EditText) findViewById(R.id.num2);
         result = (TextView) findViewById(R.id.result);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }  //onCreate()
 
     // TODO: input validation: set text to show error
     public void addNums(View v) {
         num1 = Double.parseDouble(etNum1.getText().toString());
         num2 = Double.parseDouble(etNum2.getText().toString());
+
+        //validation method
+        // needs to be a digit an not empty
+
+        if (etNum1.length() == 0 || etNum2.length() == 0  ) {
+            result.setText(R.string.invalid_number);
+        }
         result.setText(Double.toString(num1 + num2));
     }  //addNums()
 
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
