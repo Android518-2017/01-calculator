f5ae2ef01828448ae3f0e62233200e21b247bc9c
Nicolas Mazzone <n.mazzone@me.com>
2017-09-01 17:16:13 -0400

    Done steps up and to 11

--- app/src/main/java/ca/campbell/simplecalc/MainActivity.java
+++ app/src/main/java/ca/campbell/simplecalc/MainActivity.java
@@ -1,22 +1,25 @@
 package ca.campbell.simplecalc;
 
+import android.renderscript.Double2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
 
-//  TODO: add buttons & methods for subtract, multiply, divide
-//  TODO: extra input validation: no divide by zero
+        //TODO change the colour of the text
 
-//  TODO: add a clear button that will clear the result & input fields
+        //TODO change the colour of the buttons
 
-//  TODO: the hint for the result widget is hard coded, put it in the strings file
+        //TODO As you make your changes, be sure to push to your fork.
 
 public class MainActivity extends AppCompatActivity {
     EditText etNum1, etNum2;
     TextView result;
     double num1, num2;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +31,80 @@ public class MainActivity extends AppCompatActivity {
         result = (TextView) findViewById(R.id.result);
     }  //onCreate()
 
-    // TODO: input validation: set text to show error
     public void addNums(View v) {
-        num1 = Double.parseDouble(etNum1.getText().toString());
-        num2 = Double.parseDouble(etNum2.getText().toString());
+        boolean valid1 = validateEntry(etNum1.getText().toString(),etNum1);
+        boolean valid2 = validateEntry(etNum2.getText().toString(),etNum2);
+
+        if(valid1 && valid2) {
+            num1 = Double.parseDouble(etNum1.getText().toString());
+            num2 = Double.parseDouble(etNum2.getText().toString());
+        }
+
         result.setText(Double.toString(num1 + num2));
     }  //addNums()
+
+    public void subNums(View v) {
+        boolean valid1 = validateEntry(etNum1.getText().toString(),etNum1);
+        boolean valid2 = validateEntry(etNum2.getText().toString(),etNum2);
+
+        if(valid1 && valid2) {
+            num1 = Double.parseDouble(etNum1.getText().toString());
+            num2 = Double.parseDouble(etNum2.getText().toString());
+        }
+
+        result.setText(Double.toString(num1 - num2));
+    }
+
+    public void multNums(View v) {
+        boolean valid1 = validateEntry(etNum1.getText().toString(),etNum1);
+        boolean valid2 = validateEntry(etNum2.getText().toString(),etNum2);
+
+        if(valid1 && valid2) {
+            num1 = Double.parseDouble(etNum1.getText().toString());
+            num2 = Double.parseDouble(etNum2.getText().toString());
+        }
+
+        result.setText(Double.toString(num1 * num2));
+    }
+
+    public void divNums(View v) {
+        boolean valid1 = validateEntry(etNum1.getText().toString(),etNum1);
+        boolean valid2 = validateEntry(etNum2.getText().toString(),etNum2);
+
+        if(valid1 && valid2) {
+            num1 = Double.parseDouble(etNum1.getText().toString());
+            num2 = Double.parseDouble(etNum2.getText().toString());
+        }
+
+        if(num2 == 0){
+            result.setText(R.string.zero_divide);
+        }
+        else {
+            result.setText(Double.toString(num1 / num2));
+        }
+    }
+
+    public void clearAll(View v){
+        etNum1.setText("");
+        etNum2.setText("");
+        result.setText("");
+    }
+
+    private boolean validateEntry(String entry, EditText etNum){
+        if(entry.equals("")){
+            etNum.setText(R.string.invalid_entry);
+            return false;
+        }
+        else{
+            try {
+                Double.parseDouble(etNum.getText().toString());
+            }catch (NumberFormatException e){
+                Log.w(TAG, "Parsing error: " + e.toString());
+                etNum.setText(R.string.invalid_entry);
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
